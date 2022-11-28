59e9ea264294d7f65566c44e1b288440f66501a3
Raku Zeta <zetaraku@gmail.com>
2022-11-28 16:16:48 +0800

    🔧 config vite to use unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react'
-    })
+    }),
+    unocss()
   ]
 })
