57bd73bc84e8d00d2e3747416f2a256c6fb41f5e
Raku Zeta <zetaraku@gmail.com>
2022-11-28 14:40:14 +0800

    🩹 import react from default export

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
     <div>
