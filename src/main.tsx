bf4ba3ef253888c2fc2a620dfad225331f061182
Raku Zeta <zetaraku@gmail.com>
2022-12-05 21:52:03 +0800

    💄 add Roboto font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import * as M from '@mui/material'
 import App from './App'
+import '@fontsource/roboto/500.css'
 import 'virtual:uno.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
