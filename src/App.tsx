ff5296de28dc934b67c304d99bc3f8cb2c679ad5
Raku Zeta <zetaraku@gmail.com>
2022-12-09 15:44:42 +0800

    🔥 remove example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import React from 'react'
-import { css } from '@emotion/react'
 
 const App: React.FunctionComponent = () => {
-  const [count, setCount] = React.useState(0)
-
   return (
     <div>
-      <h1 class="text-red-500">Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p
-          css={css`
-            color: blue;
-          `}
-        >
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
     </div>
   )
 }
